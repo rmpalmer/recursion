4495304a72b0978181e47fcb5a5d058cf20f3df6
Richard M. Palmer <richard.m.palmer@gmail.com>
2018-03-03 17:01:23 -0600

    exercise 7

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,25 +1,53 @@
 (ns recursion)
 
 (defn product [coll]
-  :-)
+  (if (empty? coll)
+    1
+    (* (first coll)
+       (product (rest coll)))))
 
 (defn singleton? [coll]
-  :-)
+  (if (empty? coll)
+    false
+    (if (empty? (rest coll))
+      true
+      false)))
 
 (defn my-last [coll]
-  :-)
+  (if (singleton? coll)
+    (first coll)
+    (if (empty? coll)
+      nil
+      (my-last (rest coll)))))
+
 
 (defn max-element [a-seq]
-  :-)
+  (if (empty? a-seq)
+    nil
+    (if (singleton? a-seq)
+      (first a-seq)
+      (max (first a-seq) (max-element (rest a-seq))))))
+
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (let [l1 (count seq-1)
+        l2 (count seq-2)]
+   (if (> l1 l2)
+     seq-1
+     seq-2)))
+
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    nil
+    (if (singleton? a-seq)
+      (first a-seq)
+      (seq-max (first a-seq)
+               (longest-sequence (rest a-seq))))))
+
 
 (defn my-filter [pred? a-seq]
-  [:-])
+ :-)
 
 (defn sequence-contains? [elem a-seq]
   :-)
